e52561422a3f977efe63e33dc0ec061e970a4cf5
Seena2 <dinku22@gmail.com>
2023-04-12 17:03:17 +0300

    membership form done

--- src/index.js
+++ src/index.js
@@ -4,6 +4,26 @@ import myName from "./myName";
 //testing the setup
 //alert('test passed');
 
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("main_nav");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+//component function added
 function component(){
     const element=document.createElement('div');
 
